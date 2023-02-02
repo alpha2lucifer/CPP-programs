f9b7ed1a42aa7d8aaf5ec31587dfe89d211089b6
Chandan <97820157+alpha2lucifer@users.noreply.github.com>
2023-02-02 21:35:26 +0530

    cpp file

---
+++ folder01/pgm01.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int age, day, month, year;
+    char sex;
+    cout << "enter age : " ;
+    cin >> age;
+    day = 14;
+    month = 10;
+    year = 2022;
+    sex = 'M';
+    cout << "Hello world !\n" ;
+    cout << "I\'m new to programming ;)" << endl ;
+    cout << "\nRaju Rastougi " << sex << "\nAge : " << age << "\n" ;
+    cout << "\nDate : " << day << "-" << month << "-" << year << "\n" ;
+}
